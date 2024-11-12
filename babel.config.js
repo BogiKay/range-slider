dd21b7620803d44070c2539953210cbeb65d6659
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-11-12 15:45:35 +0100

    project init

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+       [
+         'module-resolver',
+         {
+           root: [ './' ],
+           extensions: [
+             '.ios.js',
+             '.ios.ts',
+             '.ios.tsx',
+             '.android.js',
+             '.android.ts',
+             '.android.tsx',
+             '.js',
+             '.ts',
+             '.tsx',
+             '.json',
+           ],
+         },
+       ],
+     [
+        '@babel/plugin-transform-react-jsx',
+        {
+          runtime: 'automatic',
+        },
+      ],
+    ],
 };
